8cbd57473eebba30e575c19677c440590d0646db
dominikkiepura1 <dominik.kiepura1@microsoft.wsei.edu.pl>
2025-01-23 14:07:46 +0100

    Naprawa działania autentykacji

--- RentingAppFrontend/BlazorApp1/obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
+++ RentingAppFrontend/BlazorApp1/obj/Debug/net7.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46ee7bd658f4beafa4ed842074ef7c8a325d33f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4aa4c878f4a6d1e6422230ecf0412721023140")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
